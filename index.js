ceeca36fc6cc5e73898b635aab0385a370e6d579
MidasCodex <nawir.ibrahim@strathmore.edu>
2025-09-10 09:49:14 +0300

    Finished Work

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+// 1. Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // 62
+
+// 2. Random integer > 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// 3. Mod
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4; // 4
+
+// 4. Max
+const max = Math.max(1, 5, 10, 20, 15);
